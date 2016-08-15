2a02bbe0d1db1c52ab02be4073d346d6aef80074
Mirko Nasato <mirko@encoded.io>
2016-08-15 11:03:15 +0200

    upgraded to rc.5 and @NgModule

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 
 import { enableProdMode } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
 console.info('app.environment:', app.environment);
 if (app.environment === 'production') {
   enableProdMode();
 }
-bootstrap(AppComponent);
+platformBrowserDynamic().bootstrapModule(AppModule);
